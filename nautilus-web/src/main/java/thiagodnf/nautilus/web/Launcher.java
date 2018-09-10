06769e5cd417b0624b6bb753e6e30032e4961ebf
Thiago Nascimento <thiagodnf@gmail.com>
2018-09-10 17:28:24 -0300

    added timezone support

--- nautilus-web/src/main/java/thiagodnf/nautilus/web/Launcher.java
+++ nautilus-web/src/main/java/thiagodnf/nautilus/web/Launcher.java
@@ -1,11 +1,20 @@
 package thiagodnf.nautilus.web;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Launcher {
-	
+
+	@PostConstruct
+	public void initIt() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo")); 
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Launcher.class, args);
 	}
