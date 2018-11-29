56f77a029d16c40871a691e3ad5c9e23921dc63b
Thiago Nascimento <thiagodnf@gmail.com>
2018-11-29 08:28:22 -0200

    changed

--- nautilus-core/src/main/java/thiagodnf/nautilus/core/indicator/NumberOfSolutionInROI.java
+++ nautilus-core/src/main/java/thiagodnf/nautilus/core/indicator/NumberOfSolutionInROI.java
@@ -3,6 +3,7 @@ package thiagodnf.nautilus.core.indicator;
 import java.util.List;
 
 import org.uma.jmetal.qualityindicator.impl.GenericIndicator;
+import org.uma.jmetal.util.front.Front;
 import org.uma.jmetal.util.point.PointSolution;
 
 import thiagodnf.nautilus.core.algorithm.RNSGAII.PointSolutionUtils;
@@ -13,8 +14,9 @@ public class NumberOfSolutionInROI extends GenericIndicator<PointSolution> {
 	private static final long serialVersionUID = 6046908284567336401L;
 	
 	protected RMetric rMetric;
-
+		
 	public NumberOfSolutionInROI(PointSolution zr, double delta) {
+		
 		this.rMetric = new RMetric(zr, delta);
 	}
 	
