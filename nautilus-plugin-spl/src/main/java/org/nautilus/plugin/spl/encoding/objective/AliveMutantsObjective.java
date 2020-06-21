cd414bd6fc2d0e41b903f722c0ce325bdc28d94f
Thiago Nascimento <thiagodnf@gmail.com>
2020-06-21 12:21:00 -0400

    Update AliveMutantsObjective.java

--- nautilus-plugin-spl/src/main/java/org/nautilus/plugin/spl/encoding/objective/AliveMutantsObjective.java
+++ nautilus-plugin-spl/src/main/java/org/nautilus/plugin/spl/encoding/objective/AliveMutantsObjective.java
@@ -6,33 +6,11 @@ import java.util.Set;
 import org.nautilus.core.model.Instance;
 import org.nautilus.core.objective.AbstractObjective;
 import org.nautilus.plugin.spl.encoding.instance.AbstractTXTInstanceData;
-import org.uma.jmetal.solution.BinarySolution;
 import org.uma.jmetal.solution.Solution;
-import org.uma.jmetal.util.binarySet.BinarySet;
 
 public class AliveMutantsObjective extends AbstractObjective {
 
 	protected Set<Integer> deadMutants;
-	
-//	@Override
-//	public double calculate(InstanceData instanceData, Solution<?> sol) {
-//return 0.0;
-//		BinarySolution solution = (BinarySolution) sol;
-//		
-//		AbstractTXTInstanceData instance = (AbstractTXTInstanceData) instanceData;
-//
-//		BinarySet binarySet = solution.getVariableValue(0) ;
-//		
-//		Set<Integer> deadMutants = new HashSet<>();
-//
-//		for (int i = 0; i < binarySet.getBinarySetLength(); i++) {
-//			if (binarySet.get(i)) {
-//				deadMutants.addAll(instance.getMutants(i));
-//			}
-//		}
-//
-//		return 1.0 - (double) deadMutants.size() / (double) instance.getNumberOfMutants();
-//	}
 	
 	@Override
 	public void beforeProcess(Instance instanceData, Solution<?> sol) {
